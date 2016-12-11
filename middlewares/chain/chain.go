bda615d8d472d5df1c3d4160112879bbe2c73824
Siddhartha Basu <biosidd@gmail.com>
2016-12-11 19:06:49 +0000

    Reworded the documentaion to make the lint standard

--- middlewares/chain/chain.go
+++ middlewares/chain/chain.go
@@ -7,20 +7,20 @@ import (
 	"net/http"
 )
 
-// A signature type for net/http middleware
+// MiddlewareFn is a signature type for net/http middleware
 type MiddlewareFn func(http.HandlerFunc) http.HandlerFunc
 
-// A type that keeps a stack of net/http middlewares
+// Chain keeps a stack of net/http middlewares
 type Chain struct {
 	middlewares []MiddlewareFn
 }
 
-// Constructor for a new chain
+// NewChain is a constructor for a new chain
 func NewChain(m ...MiddlewareFn) Chain {
 	return Chain{append([]MiddlewareFn(nil), m...)}
 }
 
-// Terminates a middleware chain by passing the final http.HandlerFunc
+// ThenFunc terminates a middleware chain by passing the final http.HandlerFunc
 func (c Chain) ThenFunc(fn http.HandlerFunc) http.HandlerFunc {
 	idx := len(c.middlewares) - 1
 	for i := idx; i >= 0; i-- {
@@ -29,14 +29,14 @@ func (c Chain) ThenFunc(fn http.HandlerFunc) http.HandlerFunc {
 	return fn
 }
 
-// Takes a new http.HandlerFunc, appends and returns a new chain from the existing one
+// Append takes a new http.HandlerFunc, appends and returns a new chain from the existing one
 func (c Chain) Append(m ...MiddlewareFn) Chain {
 	nc := make([]MiddlewareFn, 0, len(c.middlewares)+len(m))
 	nc = append(nc, c.middlewares...)
 	return Chain{append(nc, m...)}
 }
 
-// Takes a new Chain, appends and returns a new chain from the existing one
+// Extend takes a new Chain, appends and returns a new chain from the existing one
 func (c Chain) Extend(chain Chain) Chain {
 	return c.Append(chain.middlewares...)
 }
