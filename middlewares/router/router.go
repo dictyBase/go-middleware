f053b55be4f1af4f0b6c6f760d87798e8fd9f14e
Siddhartha Basu <biosidd@gmail.com>
2016-12-12 18:07:01 +0000

    Added a separate type for context key

--- middlewares/router/router.go
+++ middlewares/router/router.go
@@ -9,57 +9,67 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-const params = "params"
+type contextKey string
 
-// Wrapper for httprouter
-type RouterWrapper struct {
+// String output the details of context key
+func (c contextKey) String() string {
+	return "router context key " + string(c)
+}
+
+// contextKeyParams is the key used for stroing router Param structure
+var (
+	contextKeyParams = contextKey("params")
+)
+
+// Wrapper is a wrapper for httprouter
+type Wrapper struct {
 	Router *httprouter.Router
 }
 
-// Creates a new instance of wrapper
-func NewRouter() *RouterWrapper {
-	return &RouterWrapper{Router: httprouter.New()}
+// NewRouter is the constructor
+func NewRouter() *Wrapper {
+	return &Wrapper{Router: httprouter.New()}
 }
 
 // Delete is a shortcut wrapper for httprouter's DELETE
 // Example: r.Delete("/exercise", handler)
-func (r *RouterWrapper) Delete(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Delete(path string, fn http.HandlerFunc) {
 	r.Router.DELETE(path, HandlerFunc(fn))
 }
 
 // Get is a shortcut wrapper for httprouter's GET
 // Example: r.Get("/exercise", handler)
-func (r *RouterWrapper) Get(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Get(path string, fn http.HandlerFunc) {
 	r.Router.GET(path, HandlerFunc(fn))
 }
 
-// Get is a shortcut wrapper for httprouter's HEAD
+// Head is a shortcut wrapper for httprouter's HEAD
 // Example: r.HEAD("/exercise", handler)
-func (r *RouterWrapper) Head(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Head(path string, fn http.HandlerFunc) {
 	r.Router.HEAD(path, HandlerFunc(fn))
 }
 
-// Get is a shortcut wrapper for httprouter's OPTIONS
+// Options is a shortcut wrapper for httprouter's OPTIONS
 // Example: r.OPTIONS("/exercise", handler)
-func (r *RouterWrapper) Options(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Options(path string, fn http.HandlerFunc) {
 	r.Router.OPTIONS(path, HandlerFunc(fn))
 }
 
-// Get is a shortcut wrapper for httprouter's PATCH
+// Patch is a shortcut wrapper for httprouter's PATCH
 // Example: r.PATCH("/exercise", handler)
-func (r *RouterWrapper) Patch(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Patch(path string, fn http.HandlerFunc) {
 	r.Router.PATCH(path, HandlerFunc(fn))
 }
 
-// Get is a shortcut wrapper for httprouter's POST
+// Post is a shortcut wrapper for httprouter's POST
 // Example: r.POST("/exercise", handler)
-func (r *RouterWrapper) Post(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Post(path string, fn http.HandlerFunc) {
 	r.Router.POST(path, HandlerFunc(fn))
 }
 
-// Get is a shortcut wrapper for httprouter's PUT
+// Put is a shortcut wrapper for httprouter's PUT
 // Example: r.PUT("/exercise", handler)
-func (r *RouterWrapper) Put(path string, fn http.HandlerFunc) {
+func (r *Wrapper) Put(path string, fn http.HandlerFunc) {
 	r.Router.PUT(path, HandlerFunc(fn))
 }
 
@@ -67,12 +77,12 @@ func (r *RouterWrapper) Put(path string, fn http.HandlerFunc) {
 // httprouter. Generally this function is not called directly.
 func HandlerFunc(fn http.HandlerFunc) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		ctx := context.WithValue(r.Context(), params, p)
+		ctx := context.WithValue(r.Context(), contextKeyParams, p)
 		fn(w, r.WithContext(ctx))
 	}
 }
 
-// Return the httprouter.Params struct from the http request
+// Params returns the httprouter.Params struct from the http request
 func Params(r *http.Request) httprouter.Params {
-	return r.Context().Value(params).(httprouter.Params)
+	return r.Context().Value(contextKeyParams).(httprouter.Params)
 }
