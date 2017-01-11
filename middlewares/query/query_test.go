1948a3adbe24c1278ea3fb7bae29e6c868ad5965
Siddhartha Basu <biosidd@gmail.com>
2017-01-11 23:09:15 +0000

    Added test for filter parameter

--- middlewares/query/query_test.go
+++ middlewares/query/query_test.go
@@ -12,6 +12,26 @@ import (
 	"github.com/dictyBase/modware/modwaretest"
 )
 
+func getfilter(w http.ResponseWriter, r *http.Request) {
+	p, ok := r.Context().Value(ContextKeyQueryParams).(*Params)
+	if ok {
+		if p.HasFilters {
+			w.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(w).Encode(p.Filters)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "no filters")
+		}
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprint(w, "No query params")
+}
+
 func getfields(w http.ResponseWriter, r *http.Request) {
 	p, ok := r.Context().Value(ContextKeyQueryParams).(*Params)
 	if ok {
@@ -31,6 +51,7 @@ func getfields(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusBadRequest)
 	fmt.Fprint(w, "No query params")
 }
+
 func getinclude(w http.ResponseWriter, r *http.Request) {
 	p, ok := r.Context().Value(ContextKeyQueryParams).(*Params)
 	if ok {
@@ -45,6 +66,28 @@ func getinclude(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusBadRequest)
 	fmt.Fprint(w, "No query params")
+}
+
+func TestFilter(t *testing.T) {
+	testHandlerFn := http.HandlerFunc(getfilter)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(
+		"GET",
+		"http://dictybase.org/example?filter[name]=foo&filter[country]=argentina",
+		nil,
+	)
+	r.Header.Set(acceptH, filterMediaType)
+	r.Header.Set(contentType, filterMediaType)
+	MiddlewareFn(testHandlerFn).ServeHTTP(w, r)
+	if w.Code != http.StatusOK {
+		t.Fatalf("unexpected http response %d with body %s", w.Code, w.Body.String())
+	}
+	jsonBlob := []byte(`{"country":"argentina", "name": "foo"}`)
+	expJSON := modwaretest.IndentJSON(jsonBlob)
+	matchJSON := modwaretest.IndentJSON(w.Body.Bytes())
+	if bytes.Compare(expJSON, matchJSON) != 0 {
+		t.Fatalf("expected \n%s response does not match with \n%s\n", string(expJSON), string(matchJSON))
+	}
 }
 
 func TestFields(t *testing.T) {
