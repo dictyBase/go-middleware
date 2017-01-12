d52e82b50784514933d0110778b74a3d6c0caac1
Siddhartha Basu <biosidd@gmail.com>
2017-01-12 21:42:49 +0000

    Added test for check error condition for filter query parameter

--- middlewares/query/query_test.go
+++ middlewares/query/query_test.go
@@ -12,6 +12,13 @@ import (
 	"github.com/dictyBase/modware/modwaretest"
 )
 
+// IndentJSON uniformly indent the json byte
+func IndentJSON(b []byte) []byte {
+	var out bytes.Buffer
+	_ = json.Indent(&out, b, "", " ")
+	return bytes.TrimSpace(out.Bytes())
+}
+
 func getfilter(w http.ResponseWriter, r *http.Request) {
 	p, ok := r.Context().Value(ContextKeyQueryParams).(*Params)
 	if ok {
@@ -66,6 +73,34 @@ func getinclude(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusBadRequest)
 	fmt.Fprint(w, "No query params")
+}
+
+func TestFilterError(t *testing.T) {
+	testHandlerFn := http.HandlerFunc(getfilter)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(
+		"GET",
+		"http://dictybase.org/example?filter[name]=foo&filter[country]=argentina",
+		nil,
+	)
+	MiddlewareFn(testHandlerFn).ServeHTTP(w, r)
+	if w.Code != http.StatusNotAcceptable {
+		t.Fatalf("unexpected http response %d with body %s", w.Code, w.Body.String())
+	}
+	jsonBlob := []byte(`{
+				"errors":[
+					{
+						"status":"406","title":"Accept header is not acceptable",
+						"detail":"Not Acceptable: The given Accept header value  is incorrect for filter query extension",
+						"meta":{"creator":"api error helper"}
+					}
+				]
+			}`)
+	expJSON := IndentJSON(jsonBlob)
+	matchJSON := IndentJSON(w.Body.Bytes())
+	if bytes.Compare(expJSON, matchJSON) != 0 {
+		t.Fatalf("expected \n%s response does not match with \n%s\n", string(expJSON), string(matchJSON))
+	}
 }
 
 func TestFilter(t *testing.T) {
