350dff671b4cf18bcfa1ad1b9b9c8a9e4828e1ba
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-10-27 18:51:35 -0500

    refactor:switch to http.Handler

--- middlewares/chain/chain.go
+++ middlewares/chain/chain.go
@@ -1,6 +1,5 @@
 // Package chain provides types and functions to compose net/http compatible middlewares.
-// It is completely borrowed from alice(https://godoc.org/github.com/justinas/alice) except it only
-// works with http.HandlerFunc.
+// It is completely borrowed from alice(https://godoc.org/github.com/justinas/alice)
 package chain
 
 import (
@@ -8,7 +7,7 @@ import (
 )
 
 // MiddlewareFn is a signature type for net/http middleware
-type MiddlewareFn func(http.HandlerFunc) http.HandlerFunc
+type MiddlewareFn func(http.Handler) http.Handler
 
 // Chain keeps a stack of net/http middlewares
 type Chain struct {
@@ -20,8 +19,8 @@ func NewChain(m ...MiddlewareFn) Chain {
 	return Chain{append([]MiddlewareFn(nil), m...)}
 }
 
-// ThenFunc terminates a middleware chain by passing the final http.HandlerFunc
-func (c Chain) ThenFunc(fn http.HandlerFunc) http.HandlerFunc {
+// Then terminates a middleware chain by passing the final http.Handler
+func (c Chain) Then(fn http.Handler) http.Handler {
 	idx := len(c.middlewares) - 1
 	for i := idx; i >= 0; i-- {
 		fn = c.middlewares[i](fn)
@@ -29,7 +28,7 @@ func (c Chain) ThenFunc(fn http.HandlerFunc) http.HandlerFunc {
 	return fn
 }
 
-// Append takes a new http.HandlerFunc, appends and returns a new chain from the existing one
+// Append takes a new http.Handler, appends and returns a new chain from the existing one
 func (c Chain) Append(m ...MiddlewareFn) Chain {
 	nc := make([]MiddlewareFn, 0, len(c.middlewares)+len(m))
 	nc = append(nc, c.middlewares...)
