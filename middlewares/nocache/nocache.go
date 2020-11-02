78062dad1e4b68eed4ade96665d72bb020734564
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-11-02 15:49:27 -0600

    refactor:rename Handler to Middleware method

--- middlewares/nocache/nocache.go
+++ middlewares/nocache/nocache.go
@@ -17,12 +17,12 @@ var noCacheHeaders = map[string]string{
 	"X-Accel-Expires": "0",
 }
 
-// Handler as per http://wiki.nginx.org/HttpProxyModule. It sets:
+// Middleware as per http://wiki.nginx.org/HttpProxyModule. It sets:
 //      Expires: Thu, 01 Jan 1970 00:00:00 UTC
 //      Cache-Control: no-cache, private, max-age=0
 //      X-Accel-Expires: 0
 //      Pragma: no-cache (for HTTP/1.0 proxies/clients)
-func Handler(next http.Handler) http.Handler {
+func Middleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		// Set our NoCache headers
 		for k, v := range noCacheHeaders {
